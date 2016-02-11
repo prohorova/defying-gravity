fd331a8e5b7814e4967da4aaab2e9c72b44e539c
prohorova <prokhorova.ann26@gmail.com>
2016-02-11 20:23:51 +0400

    init project

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name defyingGravityApp
+ * @description
+ * # defyingGravityApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('defyingGravityApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name defyingGravityApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the defyingGravityApp
+ */
+angular.module('defyingGravityApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
